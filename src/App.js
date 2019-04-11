100b7e3a5868825bd3023614425f37701581ebcb
pezhed <ericlopez07@gmail.com>
2019-04-11 07:01:48 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+  	        "number": {
+  	            "value": 200
+  	        },
+  	        "size": {
+  	            "value": 3
+  	        }
+  	    }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles"
+              params={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {
+    //
+    //
+    //   <FaceRecognition />
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
